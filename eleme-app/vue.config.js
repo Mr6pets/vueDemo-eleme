55731567df4421666e5e9a957e2291d9c26a0aaf
liuan <1761330769@qq.com>
2019-12-07 22:14:48 +0800

    ..

--- eleme-app/vue.config.js
+++ eleme-app/vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    http: false,
+    hotOnly: false,
+    proxy: {
+      //配置跨域
+      '/api': {
+        target: 'https://ele-interface.herokuapp.com/api/',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
+    before: app => { }
+  }
 }
